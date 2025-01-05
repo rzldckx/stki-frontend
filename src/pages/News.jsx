d85fe8c48d50dc32d25a8612b16e4fa147f2ed49
Rizalda <rizaldadickx@gmail.com>
2025-01-05 15:35:06 +0700

    fix news

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
+import fetchData from "../api/News";
 import Card from "../components/Card";
+import Pagination from "../components/Pagination";
 import Loader from "../components/Loader"; // Import the Loader component
-import Pagination from "../components/Pagination"; // Import the Pagination component
 
-const SearchPage = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
+const News = ({ category }) => {
   const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(false); // Add loading state
   const [totalPages, setTotalPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
-  const keyword = location.state?.keyword || "";
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
@@ -21,71 +21,65 @@ const SearchPage = () => {
   }, [location.search]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const getData = async () => {
       setLoading(true); // Set loading to true before fetching data
-      try {
-        const response = await axios.get(
-          `${
-            import.meta.env.VITE_API_URL
-          }/search?q=${keyword}&page=${currentPage}`
-        );
-        console.log("Fetched data:", response.data); // Log fetched data
-        setData(response.data.news);
-        setTotalPages(response.data.total_pages);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setLoading(false); // Set loading to false after fetching data
+      const { data, total_pages, error } = await fetchData({
+        category,
+        page: currentPage,
+      });
+      if (error) {
+        setError(error);
+      } else {
+        setData(data);
+        setTotalPages(total_pages);
+        console.log("Total hlm: " + total_pages);
       }
+      setLoading(false); // Set loading to false after fetching data
     };
 
-    if (keyword) {
-      fetchData();
-    } else {
-      setLoading(false);
-    }
-  }, [keyword, currentPage]);
+    getData();
+  }, [category, currentPage]);
 
   const handlePageChange = (page) => {
     navigate(`?page=${page}`);
   };
 
-  if (loading) {
-    return <Loader />; // Use Loader component
+  if (error) {
+    return <div className="text-red-500">Error fetching data: {error}</div>;
   }
 
   return (
-    <div className="w-full">
-      <div className="max-w-screen-xl mx-auto w-full px-4 lg:px-8">
-        <div className="font-black text-[#F60E2A] text-xl lg:text-2xl lg:ml-4 mb-4">
-          HASIL PENCARIAN
+    <div className="w-full h-full font-mulish">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="font-black text-[#F60E2A] text-xl lg:text-2xl mb-4">
+          NEWS
         </div>
-        <div className="text-lg lg:text-xl lg:ml-4 mb-4">
-          Hasil pencarian untuk kata kunci &quot;{keyword}&quot;
-        </div>
-        <div className="ml-1 grid grid-cols-2 gap-4 pb-4 md:ml-3 lg:pb-0 lg:grid-cols-4 mt-4">
-          {Array.isArray(data) && data.length > 0 ? (
-            data.map((item) => (
-              <Card
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                image={item.image}
-                category={item.category}
-              />
-            ))
-          ) : (
-            <p>No results found</p>
-          )}
-        </div>
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
+        {loading ? ( // Show loader if loading is true
+          <Loader />
+        ) : (
+          <>
+            {/* Render data */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+              {data.map((item) => (
+                <Card
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  image={item.image}
+                  category={item.category}
+                />
+              ))}
+            </div>
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages} // Use totalPages from state
+              onPageChange={handlePageChange}
+            />
+          </>
+        )}
       </div>
     </div>
   );
 };
 
-export default SearchPage;
+export default News;
